c10927ca52528b3f534e57607153de566d06af35
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-28 11:18:17 +0300

    Homework230822 Task43 solution for x between 10**8

--- HW230822_Task43/Twographics.cs
+++ HW230822_Task43/Twographics.cs
@@ -13,14 +13,15 @@ namespace TwoGraphics
             Console.Clear();
             Console.WriteLine("Задача на построение двух линейных графиков: y = k1 * x + b1.");
             Console.WriteLine("Вам нужно будет ввести k1, b1, k2, b2 для двух линейных графиков.\n");
+            // Вводим данные 
             InitialSettings initGraph1 = new InitialSettings(0);
             InitialSettings initGraph2 = new InitialSettings(2);
             Console.WriteLine($"Вы ввели b1 {initGraph1.b1}, k1 {initGraph1.k1}");
             Console.WriteLine($"Вы ввели b2 {initGraph2.b1}, k2 {initGraph2.k1}");
-
+            //  Строим графики y = k * x + b;
             GraphSettig graph1 = new GraphSettig(initGraph1);
             GraphSettig graph2 = new GraphSettig(initGraph2);
-
+            // Ищем точку пересечения
             FindCrossing cross = new FindCrossing(graph1, graph2);
         }
     }
